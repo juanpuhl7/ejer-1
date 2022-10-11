257ab81a636328f34dac8ea4d52a4b59f8e9a3a8
johnmaster <juanpuhl404@gmail.com>
2022-10-10 22:01:42 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ejer_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cursoName, turno;
+            int cantAlumnos=0;
+            int nota=0;
+            float totalNotas=0f;
+            float promedio=0f;
+            
+            
+            Console.WriteLine(" - - SISTEMA DE NOTAS - -");
+            Console.WriteLine("Ingrese nombre del curso: ");
+            cursoName=Console.ReadLine();
+            Console.WriteLine("Ingrese Turno: ");
+            turno=Console.ReadLine();
+
+            Console.WriteLine("Cantidad de Alumnos: ");
+            cantAlumnos=Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+
+            for(int i=0; i<cantAlumnos; i++){
+                Console.WriteLine("Ingrese Nota: ");
+                nota=Convert.ToInt32(Console.ReadLine());
+                totalNotas=totalNotas+nota;
+                Console.Clear();
+            }
+
+            promedio=((float)totalNotas/(float)cantAlumnos);
+            Console.WriteLine(" - - SISTEMA DE NOTAS - -");
+            Console.WriteLine("Nombre del Curso: " + " " + cursoName);
+            Console.WriteLine("  TURNO         : " + " " + turno);
+            Console.WriteLine("  PROMEDIO      : " + " " + promedio);
+
+
+        }
+    }
+}
